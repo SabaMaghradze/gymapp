5139cd39e787a106db03c0fa03e059ffc8f8f35b
Saba Maghradze <saba@Sabas-MacBook-Pro.local>
2025-11-17 13:45:59 +0400

    fixed a typo

--- src/main/java/com/gymapp/producer/WorkloadMessageProducer.java
+++ src/main/java/com/gymapp/producer/WorkloadMessageProducer.java
@@ -16,8 +16,7 @@ public class WorkloadMessageProducer {
     public void sendWorkload(WorkloadRequest req) {
         try {
             String jsonPayload = objectMapper.writeValueAsString(req);
-
-            jmsTemplate.convertAndSend("worload.queue", jsonPayload);
+            jmsTemplate.convertAndSend("workload.queue", jsonPayload);
         } catch (Exception e) {
             throw new RuntimeException("Failed to send workload payload", e);
         }
